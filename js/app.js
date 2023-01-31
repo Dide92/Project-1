ae525e460ea6ffcd28c48893504b25f42e7e65b9
Dide Aydogan <dide1992@gmail.com>
2023-01-31 15:44:38 -0500

    added class and score  method

--- js/app.js
+++ js/app.js
@@ -14,15 +14,13 @@ let plant =document.getElementById('obstacle3')
 let play = document.getElementById('startButton')
 let startBox =document.getElementById('startBox')
 let h1=document.querySelector('h1')
-
+let p=document.querySelector('p')
 
 // background-image: ;
 // background-repeat: no-repeat;
 // background-color: rgb(182, 226, 244);
 
 //--------------------------------------------------------------
-
-
 
 function startGame() {
     sun.style.display='inline';
@@ -35,21 +33,47 @@ function startGame() {
     fire2.style.display='inline';
     plant.style.display='inline';
     grass.style.display='inline';
+    p.style.display='inline';
     startBox.style.display='none';
     h1.style.display='none';
     document.body.style.backgroundImage='none';
     document.body.style.backgroundColor='rgb(17, 144, 195)';
-
     play.remove()
-   
+    Emoji.scoreResult()
 
 }
 
 play.addEventListener('click', startGame)
 
-// function GameOver() {
-//     fire1.remove()
-//     fire2.remove()
+//--------------------------------------------------------------
+class Game {
+    constructor(timer, score, jump) {
+        this.timer=0
+        this.score=0
+        this.jump=0
+    }
+    scoreResult() {
+        setInterval(() => {
+            this.score++
+document.querySelector('.score').innerHTML = this.score
+        },900)
+    }
+}
 
-// }
+const Emoji = new Game ('Jumping Emoji')
+
+
+
+
+function gameOver() {
+    fire1.remove()
+    fire2.remove()
+    plant.remove()
+    grass.remove()
+    sun.remove()
+    cloud1.remove()
+    cloud2.remove()
+    cloud3.remove()
+    cloud4.remove()
+}
 
