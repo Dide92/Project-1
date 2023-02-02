6a1997ea6f3d2a4d3b2f20df3e451f0b091b8644
Dide Aydogan <dide1992@gmail.com>
2023-02-02 12:34:39 -0500

    gameover function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 console.log('Project 1')
 
+function hello() {
+    let playerName=prompt("Please enter your name")
+    playerName=playerName[0].toUpperCase() + playerName.substring(1)
+    if (playerName != null) {
+        document.querySelector('.player').innerText = playerName +"'s "
+    }
+}
+hello()
 
 let grass =document.getElementById('grass')
 let sun =document.getElementById('sun')
@@ -8,13 +16,23 @@ let cloud2 =document.getElementById('cloud2')
 let cloud3 =document.getElementById('cloud3')
 let cloud4 =document.getElementById('cloud4')
 let happy =document.getElementById('happy')
-let fire1 =document.getElementById('obstacle1')
+let obstacle1 =document.getElementById('obstacle1')
 let fire2 =document.getElementById('obstacle2')
 let plant =document.getElementById('obstacle3')
 let play = document.getElementById('startButton')
 let startBox =document.getElementById('startBox')
 let h1=document.querySelector('h1')
 let p=document.querySelector('p')
+let end = document.getElementById('end')
+let h2 = document.querySelector('h2')
+let score=document.querySelector('.score')
+let happyBottom = parseInt(window.getComputedStyle(happy).getPropertyValue("bottom"));    //getting the style of happy emoji with getComputedStyle and bottom location of the happy emoji using getPropertyValue
+let happyLeft = parseInt(window.getComputedStyle(happy).getPropertyValue("right"));    //getting the style of happy emoji with getComputedStyle and right location of the happy emoji using getPropertyValue
+let happyWidth = parseInt(window.getComputedStyle(happy).getPropertyValue("width"));    //getting the style of happy emoji with getComputedStyle and width location of the happy emoji using getPropertyValue
+let obstacle1Bottom =parseInt(window.getComputedStyle(obstacle1).getPropertyValue("bottom"));
+let obstacle1Left =parseInt(window.getComputedStyle(obstacle1).getPropertyValue("left"));
+let obstacle1Width =parseInt(window.getComputedStyle(obstacle1).getPropertyValue("width"));
+let obstacle1Height =parseInt(window.getComputedStyle(obstacle1).getPropertyValue("height"));
 
 
 //--------------------------------------------------------------
@@ -26,7 +44,7 @@ function startGame() {
     cloud3.style.display='inline';
     cloud4.style.display='inline';
     happy.style.display='inline';
-    fire1.style.display='inline';
+    obstacle1.style.display='inline';
     fire2.style.display='inline';
     plant.style.display='inline';
     grass.style.display='inline';
@@ -37,7 +55,7 @@ function startGame() {
     document.body.style.backgroundColor='rgb(17, 144, 195)';
     play.remove()
     Emoji.scoreResult()
-    jump()
+    // jump()
 }
 
 play.addEventListener('click', startGame)
@@ -59,37 +77,69 @@ document.querySelector('.score').innerHTML = this.score
 
 const Emoji = new Game ('Jumping Emoji')
 
-window.addEventListener('click', jump)
-
 function jump() {
-        if (happy.classList=='jump') {
-            return 
-        }
-        happy.classList.add('jump');
-        setTimeout(removeJump,300)
-}
-function removeJump() {
-    happy.classList.remove('jump') 
-
+    happy.classList.add('jump');
+    setTimeout(() => {
+        happy.classList.remove('jump');
+    },500);
 }
 
-
-
-let alive = setInterval(function () {
-    let happyTop = parseInt(window.getComputedStyle(happy).getPropertyValue('top'));
-    let fire1Left = parseInt(window.getComputedStyle(fire1).getPropertyValue('left'));
-    let fire2Left = parseInt(window.getComputedStyle(fire2).getPropertyValue('left'));
-    let plantLeft = parseInt(window.getComputedStyle(plant).getPropertyValue('left'));
-    if (fire1Left < 50 && fire1Left > 0 && happyTop >= 140) {
-        return gameOver()
+document.addEventListener('keypress',() => {
+    if (!happy.classList.contains('jump')) {
+jump();
     }
-},10)
+})
 
+
+// function jump() {
+//         if (happy.classList=='jump') {
+//             return 
+//         }
+//         happy.classList.add('jump');
+//         setTimeout(removeJump,300)
+// }
+// function removeJump() {
+//     happy.classList.remove('jump') 
+
+// }
+
+// window.addEventListener('keydown', jump)
+
+
+let alive = setInterval(() => {
+if (obstacle1Left>100) {
+    console.log('game over')
+
+}
+    // let obstacle1Left = parseInt(window.getComputedStyle(obstacle1).getPropertyValue("left"));
+    // console.log(obstacle1Left)
+    // if (obstacle1Left < 100) {
+    //     obstacle1.style.display = 'none';
+    // } else {
+    //     obstacle1.style.display = '';
+    // }
+    // let fire2Left = parseInt(window.getComputedStyle(fire2).getPropertyValue('left'));
+    // let plantLeft = parseInt(window.getComputedStyle(plant).getPropertyValue('left'));
+
+
+    // if (happyLeft>=obstacle1Right - happyWidth && happyLeft <= obstacle1Right + obstacle1Width && happyBottom <= obstacle1Bottom + obstacle1Height) {
+    //     alert("game over");
+    // }
+    //   alert("game over");
+
+},20);
+
+
+// document.addEventListener('click', function (event) {
+//     jump();
+// });
 
 
 
 function gameOver() {
-    fire1.remove()
+    end.style.display='inline';
+    h2.style.display='inline';
+    obstacle1.remove()
     fire2.remove()
     plant.remove()
     grass.remove()
@@ -98,7 +148,13 @@ function gameOver() {
     cloud2.remove()
     cloud3.remove()
     cloud4.remove()
-    document.body.style.opacity=0.5;
+    score.style.position='absolute';
+    score.style.bottom='100px';
+    document.querySelector('.player').innerText = playerName +"'s "
+    Emoji.scoreResult()
+    location.reload()
+    
+    // document.body.style.opacity=0.5;
 }
 
 
